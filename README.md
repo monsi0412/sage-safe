65601641bcf404ef50870da29651d1cf4e02cd9e
Monserrat <monserratdiaz12@gmail.com>
2017-03-15 15:12:45 +0000

    tried abstractions

---
+++ README.md
@@ -0,0 +1,18 @@
+Notes
+
+Questions:
+
+
+Need to do:
+abstraction- changes "pages"(monserrat) 
+            point algorithm (if you get it right, +1) (yanella)
+problems (solution)- 
+
+Separate Tasks: 
+Monserrat- alcohol trivia
+    first struggle: I dont know how to do my abstraction for going to the next "page"
+Yanella - substance trivia
+
+sources:
+title-screen img : http://www.clipartkid.com/drugs-and-alcohol-cliparts/
+sources for information: https://www.hhs.gov/ash/oah/adolescent-health-topics/substance-abuse/states/ny.html
--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 /* global $ */
 
 $(document).ready(function(){
-    $()
-    $("#home").hide();
     
+    $(".quest").hide();
     
+    // $("")
     
+    // var current=0;
+
+    // function nextQuestion() {
+    // var next = current + 1;
+    // var currentSel = '.quest' + current;
+    // var nextSel ='.quest' + next;
+   
+    // $(currentSel).hide();
+    // $(nextSel).show();
+   
+    // current+= 1;
+    // }
+    // $('.selection').click(function() {
+    //     nextQuestion();
+    // }); 
     
+    function nextQuestion(){
+        // so what i need to do is make it go to the next page when the button is clicked
+        var next = 
+    }
 });
