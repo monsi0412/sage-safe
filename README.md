d5a424ca628c6396d2d8e9daae83118f01044c6b
Monserrat <monserratdiaz12@gmail.com>
2017-03-20 14:23:08 +0000

    why won't jquery work

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Questions:
 Need to do:
 abstraction- changes "pages"(monserrat) 
             point algorithm (if you get it right, +1) (yanella)
+            change background color if true or false (joel)
 problems (solution)- 
 
 Separate Tasks: 
@@ -16,3 +17,4 @@ Yanella - substance trivia
 sources:
 title-screen img : http://www.clipartkid.com/drugs-and-alcohol-cliparts/
 sources for information: https://www.hhs.gov/ash/oah/adolescent-health-topics/substance-abuse/states/ny.html
+https://www.codecademy.com/courses/a-simple-counter/0/1
--- script.js
+++ script.js
@@ -1,29 +1,20 @@
 /* global $ */
 
-$(document).ready(function(){
+$(document).ready(function() {
+    $("#home").hide();
     
     $(".quest").hide();
     
-    // $("")
+    function next(){
+        $(".selection").click(){
+            $()
+        }
+    };
     
-    // var current=0;
-
-    // function nextQuestion() {
-    // var next = current + 1;
-    // var currentSel = '.quest' + current;
-    // var nextSel ='.quest' + next;
-   
-    // $(currentSel).hide();
-    // $(nextSel).show();
-   
-    // current+= 1;
-    // }
-    // $('.selection').click(function() {
-    //     nextQuestion();
-    // }); 
+    $("#start").click(){
+        next();
+        
+    });
     
-    function nextQuestion(){
-        // so what i need to do is make it go to the next page when the button is clicked
-        var next = 
-    }
+    
 });
